664aaf68f139a28cecfbfde2ea8889109999a79a
AsmiC18 <asmichaudhary06@gmail.com>
2026-06-06 15:04:03 +0530

    feat: added the add note feature

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,12 @@
-function Dashboard(props){
+
+function Dashboard({user,notes}){
     return(
         <div>
             <h1> Dashboard </h1>
-            <h3> Welcome to FocusHub, {props.user}!</h3>
+            <h3> Welcome to FocusHub, {user}!</h3>
+            <h3>Notes</h3>
+
+            {notes.map(note => <li>{note}</li>)}
         </div>
     )
 }
