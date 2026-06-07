9497e596229b578517afab9b752bd8dbde8d6d3b
AsmiC18 <asmichaudhary06@gmail.com>
2026-06-07 13:50:41 +0530

    feat: added the edit note feature

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,25 @@
 
-function Dashboard({user,notes,del}){
+function Dashboard({user,notes,del,edit,currId,currText,changeText,save}){
     return(
         <div>
             <h1> Dashboard </h1>
             <h3> Welcome to FocusHub, {user}!</h3>
             <h3>Notes</h3>
 
-            {notes.map(note => <li key={note.id}>{note.text} <button onClick= {()=> {del(note.id)}}>Delete</button></li>)}
+            {notes.map(
+                note => <li
+                 key={note.id}>
+                    {note.id===currId ?
+                    <div>
+                        <input
+                        value={currText}
+                        onChange= {(e)=> changeText(e.target.value)} 
+                        />
+                        <button onClick= {()=> save()}> Save</button>
+                    </div>
+                    : note.text
+                    
+                 } <button onClick= {()=> {del(note.id)}}>Delete</button> <button onClick= {()=> {edit(note)}}> Edit</button></li>)}
 
             
         </div>
