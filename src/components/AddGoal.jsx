08a8de33bf6788ac87f6f7478564787474e2cb27
AsmiC18 <asmichaudhary06@gmail.com>
2026-06-11 23:38:00 +0530

    feat:added the  edit, delete and add progress features for goal

---
+++ src/components/AddGoal.jsx
@@ -0,0 +1,59 @@
+import {useState} from 'react'
+
+function AddGoal({add}){
+    const [currGoalText,setGoalText]=useState('');
+    const [currTarget, setTarget]=useState('');
+    const [isAdding,setIsAdding]=useState(false);
+
+
+    if(isAdding){
+    return(
+        <div>
+            <p>
+            Goal title:
+            <input 
+            value={currGoalText}
+            onChange={(e)=>setGoalText(e.target.value)}
+            />
+            </p>
+            
+            <p>Target:
+            <input
+            value={currTarget}
+            onChange={(e)=>setTarget(e.target.value)}
+            />
+            </p>
+            <button className="button" onClick={()=>{add({
+                id: Date.now(),
+                text: currGoalText,
+                target: Number(currTarget),
+                current: 0
+            }      
+            )
+            setGoalText('');
+            setTarget('');
+            setIsAdding(false);
+
+            }
+            }
+            >Add</button>
+            <button className="button" onClick={()=>
+            {
+                setIsAdding(false)
+                setTarget('');
+                setGoalText('');
+
+                }
+            }>Cancel</button>
+            
+
+        </div>
+    )
+    }
+    else{
+        return(
+            <button className="button" onClick={()=> setIsAdding(true)}>+ New Goal</button>
+        )
+    }
+}
+export default AddGoal;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,9 @@
 import { FaTrash, FaEdit } from "react-icons/fa";
 
-function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,tasks,toggleStatus,delTask,editTask,
-    currTaskId,currTaskText,saveEditTask,changeTaskText,cancelEditTask}){
+function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,
+    tasks,toggleStatus,delTask,editTask, currTaskId,currTaskText,saveEditTask,changeTaskText,cancelEditTask,
+    goals,currGoalProgress,addProgress,currGoalId,saveProgress,newProgress,cancelAddProgress,delGoal,editGoal,
+    currEditGoalId,changeGoalText,currGoalText,currGoalTarget,changeGoalTarget,saveEditGoal,cancelEditGoal}){
   
     return(
         <div className="dashboard">
@@ -14,7 +16,7 @@ function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,t
 
                 {notes.map(
                     note =>
-                        <li className="notecard"
+                        <div className="notecard"
                     key={note.id}>
                         {note.id===currId ?
                         <div>
@@ -36,7 +38,7 @@ function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,t
                         </div>
                         </>
                     }
-                    </li>)
+                    </div>)
                 }
                 </>
             )
@@ -47,6 +49,7 @@ function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,t
                 </>
                 )
             }
+
             {tasks.length==0 ? (
                 <>
                 <h3>No tasks yet</h3>
@@ -58,7 +61,7 @@ function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,t
                     
                    { tasks.map(task=>
                     
-                    <li className="taskcard"
+                    <div className="taskcard"
                     key= {task.id}>
                         
                     {
@@ -95,13 +98,91 @@ function Dashboard({user,notes,del,edit,currId,currText,changeText,save,cancel,t
     
                     
                         
-                    </li>
+                    </div>
                     )}
                    
                   
                     </>
                 )
             
+            }
+
+            {goals.length==0 ? (
+                <h3>No Goals yet</h3>
+
+            )
+            :
+            (
+             <>
+             <h3>Goals</h3>
+                  {goals.map(goal=>
+
+                    <div className='goalcard' 
+                    key={goal.id}>
+                        {goal.id==currGoalId ? 
+                        (
+                            <>
+                            <p>{goal.text} | Target: {goal.target} | Current: {goal.current}</p>
+                            <input 
+                            type= "number"
+                            value={currGoalProgress}
+                            onChange= {(e)=> newProgress(e.target.value)}
+                            />
+                            <button onClick={()=>saveProgress()}>Save</button>
+                            <button onClick={()=>cancelAddProgress()}>Cancel</button>
+
+                            </>
+                        )
+                        : goal.id== currEditGoalId ? (
+                            <>
+                            <input 
+                            value={currGoalText}
+                            onChange={(e)=>changeGoalText(e.target.value)}
+                            />
+                            <input
+                            type="number"
+                            value= {currGoalTarget}
+                            onChange={(e)=>changeGoalTarget(e.target.value)}
+                            />
+                            <button onClick={()=>saveEditGoal()}>Save</button>
+                            <button onClick={()=>cancelEditGoal()}>Cancel</button>
+
+                            
+
+                            </>
+                        )
+                        
+                        :
+                        <>
+                            <p>{goal.text} | Target: {goal.target} | Current: {goal.current}</p>
+                            <p>Progress: {
+                                goal.target> 0 ? (Math.round((goal.current / goal.target) *100))
+                                : 
+                                0
+                            }%</p>
+                                
+                            
+
+                            <div className="goalbuttons">
+                            <button onClick={()=>addProgress(goal)}>Add Progress</button>
+                            <div className="icon">
+                            <button onClick={()=>delGoal(goal.id)}><FaTrash/></button>
+                            <button onClick={()=>editGoal(goal)}><FaEdit/></button>
+                            </div>
+                            </div>
+
+                        </>
+                        }
+
+                  
+
+                    </div>
+                    )
+                }
+             </>  
+                
+            )
+
             }
 
             
